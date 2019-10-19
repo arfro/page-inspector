1b72be363826eeab467b0f013e84ba4534a75389
annette <aerfromenes@gmail.com>
2019-10-19 23:47:20 +0200

    added all tests for HtmlInspectorService

--- test/functional/HtmlInspectionServiceSpec.scala
+++ test/functional/HtmlInspectionServiceSpec.scala
@@ -79,4 +79,99 @@ class HtmlInspectionServiceSpec extends FunctionalTest {
     }
   }
 
+  "HtmlInspectionService#getAllHeadings" should {
+    "return data for all headings" in {
+      val allHeadings = Map("h1" -> 0, "h2" -> 0, "h3" -> 0, "h4" -> 0, "h5" -> 0, "h6" -> 0 )
+      Mockito.when(exampleDocMock.childNodes()).thenReturn(childNodesMock)
+      Mockito.when(examplePageMock.doc).thenReturn(exampleDocMock)
+      Mockito.when(JSoupInspectorServiceMock.getAllHeadings(exampleDocMock)).thenReturn(allHeadings)
+      val result = htmlInspectionServiceUT.getAllHeadings(examplePageMock)
+
+      result mustBe allHeadings
+    }
+  }
+
+  "HtmlInspectionService#getAllLinksGroupedByDomain" should {
+    "group links starting in / or # or containing hostname. as internal" in {
+      val allLinks = List("/internalLink", "#internalLink", "www.hostName.com/info")
+      val hostName = "hostName"
+      Mockito.when(examplePageMock.hostName).thenReturn(Some(hostName))
+      Mockito.when(examplePageMock.doc).thenReturn(exampleDocMock)
+      Mockito.when(JSoupInspectorServiceMock.getAllLinks(exampleDocMock)).thenReturn(allLinks)
+
+      val result = htmlInspectionServiceUT.getAllLinksGroupedByDomain(examplePageMock)
+
+      result.get("internal").get mustBe allLinks
+      result.get("external").get.length mustBe 0
+    }
+  }
+
+  it should {
+    "group external links correctly" in {
+      val allLinks = List("www.linkedin.com", "www.google.com")
+      val hostName = "hostName"
+      Mockito.when(examplePageMock.hostName).thenReturn(Some(hostName))
+      Mockito.when(examplePageMock.doc).thenReturn(exampleDocMock)
+      Mockito.when(JSoupInspectorServiceMock.getAllLinks(exampleDocMock)).thenReturn(allLinks)
+
+      val result = htmlInspectionServiceUT.getAllLinksGroupedByDomain(examplePageMock)
+
+      result.get("internal").get.length mustBe 0
+      result.get("external").get mustBe allLinks
+    }
+  }
+
+  it should {
+    "group external and internal links correctly" in {
+      val allLinks = List("www.linkedin.com", "www.google.com", "/internalLink", "#internalLink", "www.hostName.com/infor", "hostName.com/about")
+      val hostName = "hostName"
+      Mockito.when(examplePageMock.hostName).thenReturn(Some(hostName))
+      Mockito.when(examplePageMock.doc).thenReturn(exampleDocMock)
+      Mockito.when(JSoupInspectorServiceMock.getAllLinks(exampleDocMock)).thenReturn(allLinks)
+
+      val result = htmlInspectionServiceUT.getAllLinksGroupedByDomain(examplePageMock)
+
+      result.get("internal").get.length mustBe 4
+      result.get("external").get.length mustBe 2
+    }
+  }
+
+  it should {
+    "handle no links in the page correctly" in {
+      val allLinks = List()
+      val hostName = "hostName"
+      Mockito.when(examplePageMock.hostName).thenReturn(Some(hostName))
+      Mockito.when(examplePageMock.doc).thenReturn(exampleDocMock)
+      Mockito.when(JSoupInspectorServiceMock.getAllLinks(exampleDocMock)).thenReturn(allLinks)
+
+      val result = htmlInspectionServiceUT.getAllLinksGroupedByDomain(examplePageMock)
+
+      result.get("internal").get.length mustBe 0
+      result.get("external").get.length mustBe 0
+    }
+  }
+
+  "HtmlInspectionService#containsLoginForm" should {
+    "handle login form detection if login form exists" in {
+      Mockito.when(examplePageMock.doc).thenReturn(exampleDocMock)
+      Mockito.when(JSoupInspectorServiceMock.containsLoginForm(exampleDocMock)).thenReturn(true)
+
+      val result = htmlInspectionServiceUT.containsLoginForm(examplePageMock)
+
+      result mustBe true
+    }
+  }
+
+  it should {
+    "handle login form detection if login form does not exist" in {
+      Mockito.when(examplePageMock.doc).thenReturn(exampleDocMock)
+      Mockito.when(JSoupInspectorServiceMock.containsLoginForm(exampleDocMock)).thenReturn(false)
+
+      val result = htmlInspectionServiceUT.containsLoginForm(examplePageMock)
+
+      result mustBe false
+    }
+  }
+
+
 }
