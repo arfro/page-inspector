86f915828399ee7fb1a449d57da31a9e768431cd
annette <aerfromenes@gmail.com>
2019-10-23 22:37:56 +0200

    added a few notes and slightly improved the code

--- app/services/linkvalidation/LinkValidationService.scala
+++ app/services/linkvalidation/LinkValidationService.scala
@@ -5,11 +5,9 @@ import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
 import config.AppConfig
 import javax.inject.Inject
-import play.api.http.Status
 import play.api.libs.ws._
 
 import scala.concurrent.Future
-import scala.util.{Failure, Success, Try}
 
 
 trait ValidationResult
@@ -20,44 +18,51 @@ class LinkValidationService @Inject() (ws: WSClient)  {
 
   // TODO: inject execution context
 
-  def validate(listOfUrls: List[String]) = batchProcess(listOfUrls, AppConfig.urlsPerBatch)
+  def validate(listOfUrls: List[String]): Future[List[ValidationResult]] = batchProcess(listOfUrls, AppConfig.urlsPerBatch)
 
-  private def batchProcess(listOfUrls: List[String], urlsPerBatch: Int = 20) = {
-    // divide into batches
+  // 1. divide links into batches
+  // 2. wrap each batch in future for parallel execution
+  // 3. for each batch send a request and get a response and then wrap it in ValidationResult case class
+  // 4. add batch results to result accumulator for foldLeft - that at the end will return one Future[List[ValidationResult]]
+  //
+
+  // TODO: fix naming of those functions
+  private def batchProcess(listOfUrls: List[String], urlsPerBatch: Int = 50): Future[List[ValidationResult]] = {
+    // divide links into batches, amount of links per batch is configurable via config with a default of 50
     val batches = listOfUrls.grouped(urlsPerBatch).toList
     println(s"Processing ${batches.length} batch(es)")
-    // wrap each batch into a future if not empty then perform process batch
-    val asyncBatch: Future[List[ValidationResult]] =
-      batches.foldLeft(Future.successful(List.empty[ValidationResult]))(processBatch)
-    // batches.flatMap(batch => processBatch(batch))
-    asyncBatch
+
+    // i want to wrap each batch in a Future to execute in parallel, using foldLeft because i want to have a different return type than 'batches' had
+    // "on the left" we will have Future[List[ValidationResult]] and "on the right" List[String]
+    batches.foldLeft(Future.successful(List.empty[ValidationResult]))(processBatch)
+
   }
 
-  private def processBatch(results: Future[List[ValidationResult]], batch: List[String]): Future[List[ValidationResult]] = {
+  // as per foldLeft this function operates on "left" side which is already of type "Future[List[ValidationResult]] and "right" which is still of "old" type
+  // to be able to fold the result type of this functoin obvs needs to be of the same return type as the "new" type
+  private def processBatch(responsesFoldAccumulator: Future[List[ValidationResult]], batch: List[String]): Future[List[ValidationResult]] = {
 
-    results.flatMap{ responses =>
-    val batchFutures: List[Future[ValidationResult]] = batch.map(url => {
-      ws.url(url).withRequestTimeout(2000.millis).get().map {
-          case d => ValidationSuccess(url, d.status)
-        }.recover{case e: Exception => ValidationFailure(url, e.getMessage)}
-    })
+    responsesFoldAccumulator.flatMap { responses =>
+      val batchFutures: List[Future[ValidationResult]] = batch.map(url => {
+        (for {
+            response <- {
+              println(s"About to send req for $url")
+              ws.url(url).withRequestTimeout(2000.millis).get()
+            }
+            _ = println(s"Response for $url received")
+            result = ValidationSuccess(url, response.status)
+         } yield result)
+          .recover{case e: Exception => ValidationFailure(url, e.getMessage)}
+      })
 
-    val batchFuture: Future[List[ValidationResult]] = Future.sequence(batchFutures)
+      // generating one big Future out of all sequences of Futures so I can map below and add to the accumulator
+      val batchFuture: Future[List[ValidationResult]] = Future.sequence(batchFutures)
 
-    batchFuture.map { batchResponses =>
-      println("Finished a batch")
-      responses ++ batchResponses
-    }}
-//
-//    val batchFuture: List[Future[ValidationResult]] = batchFutures
-//
-//    val res2: Future[List[ValidationResult]] = for {
-//      result <- results
-//      ba <- batchFuture
-//    } yield result ++ ba
-//      res2
-
-
+      batchFuture.map { batchResponses =>
+        println("Batch processed")
+        responses ++ batchResponses
+      }
+    }
   }
 
 
--- untitled.sc
+++ untitled.sc
@@ -1,6 +1,10 @@
+val l = List(List(1), List(2), List(3)).grouped(3).toList
 
+def lint(left: List[String], right: List[List[Int]]): List[String] = {
+  right.map({f =>
+    println(s"processing $f")
+      f.toString + "lala"
+  })
+}
 
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
-
-ws.url(url).withRequestTimeout(10000.millis).get()
+l.foldLeft(List.empty[String])(lint)
